def51824beebb8c98974a7e4ac15ce43d82ec3d1
Fenix912 <maggie991@bk.ru>
2022-02-09 13:20:09 +0300

    Zadacha-2.3.1

---
+++ src/main/java/web/service/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package web.service;
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.dao.UserDao;
+import web.model.User;
+
+import java.util.List;
+@Service
+@Transactional                                                                                       //методы класса должены выполняться в транзакции
+public class UserServiceImpl implements UserService {
+
+    private final UserDao userDao;
+
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    public User getUserById(long id) {
+        return userDao.getUserById(id);
+    }
+
+    @Override
+    public void add(User user) {
+        userDao.add(user);
+    }
+
+    @Override
+    public void update(Long id, User newUser) {
+        userDao.update(id, newUser);
+    }
+
+    @Override
+    public void delete(long id) {
+        userDao.delete(id);
+    }
+}
